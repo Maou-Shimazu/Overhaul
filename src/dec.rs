819b88120316955bf28c32d84ff79a9c50932aad
Eldad Danladi <eldaddanladi@gmail.com>
2022-03-20 13:17:03 -0600

    added more configuration files for multi links

--- src/dec.rs
+++ src/dec.rs
@@ -1,17 +1,18 @@
 use configparser::ini::Ini;
-use std::fs::OpenOptions;
-use std::io::{stdout, Write};
-use text_io::read;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::path::Path;
 use std::error::Error;
+use std::fs::File;
+use std::fs::OpenOptions;
+use std::io::{self, BufRead};
+use std::io::{stdout, Write};
+use std::path::Path;
+use text_io::read;
+
 // use colored::*; //to add colors
 
 // static path: &str = "{path_goes_here}/config/overhaul.ini";
 pub static PATH: &str = "C:/Users/User5/Documents/Github Projects/Overhaul/config/overhaul.ini";
-pub static LOG: &str = "C:/Users/User5/Documents/Github Projects/Overhaul/config/log.ini";
-
+pub static URL: &str = "C:/Users/User5/Documents/Github Projects/Overhaul/config/url.ini";
+pub static LOC: &str = "C:/Users/User5/Documents/Github Projects/Overhaul/config/loc.ini";
 /// Get specific key from overhaul.ini. takes a String which is assigned config.get() and returns the key for usage;
 pub fn get_specific_key(key: String) -> String {
     key
@@ -47,8 +48,8 @@ pub fn add_new() -> Result<(), std::io::Error> {
     let fileurl: String = format!("\nurl = {}", _fileurl);
     update_configuration_string(PATH, filename);
     //update_configuration_string(LOG, filelog);
-    update_configuration_string(PATH, fileloc);
     update_configuration_string(PATH, fileurl);
+    update_configuration_string(PATH, fileloc);
     Ok(())
 }
 
@@ -57,7 +58,7 @@ pub fn get_config() {
     let contents = std::fs::read_to_string(PATH).expect("Something went wrong reading the file");
     println!("\n{}", contents);
 }
-/// stores request from get_request
+/// stores get_request
 pub fn store_request(request: String) -> String {
     request
 }
@@ -66,7 +67,7 @@ pub fn store_request(request: String) -> String {
 pub fn get_location(section: &str) -> String {
     return Ini::new().get(section.trim(), "loc").unwrap();
 }
-/// Takes filename as str and data to be writen to the file as String, to be used with store_request() 
+/// Takes filename as str and data to be writen to the file as String, to be used with store_request()
 pub fn write_to_file(file: &str, data: String) {
     let mut write = OpenOptions::new()
         .write(true)
@@ -79,8 +80,6 @@ pub fn write_to_file(file: &str, data: String) {
 }
 
 pub fn update_file() -> Result<(), Box<dyn Error>> {
-    let mut config = Ini::new();
-    let _overhaul = config.load(PATH)?;
     println!("\nPlease make sure the name of file is the same as when you registered it, if you dont remember then select 4 from main menu.");
     print!("\nName of file you want to update: ");
     stdout().flush().ok();
@@ -91,17 +90,45 @@ pub fn update_file() -> Result<(), Box<dyn Error>> {
     let _overhaul = config.load(PATH)?;
     let url = config.get(section.as_str().trim(), "url").unwrap();
     let request = reqwest::blocking::get(url)?.text()?;
-    //let request = reqwest::get(url).await?.text().await?;
     let request = store_request(request);
     let loc = config.get(section.as_str().trim(), "loc").unwrap();
     write_to_file(loc.as_str(), request);
     Ok(())
 }
 
-pub fn update_all(){
-    
+pub fn edit_configuration() -> Result<(), Box<dyn Error>> {
+    let mut config = Ini::new();
+    let _overhaul = config.load(PATH)?;
+    print!("\nName of file you want to configure: ");
+    stdout().flush().ok();
+    let section: String = read!();
+    print!("Section you want to configure:\n[1] Url \n[2] Location \n[3] Both: ");
+    stdout().flush().ok();
+    let configuration: i32 = read!();
+    match configuration {
+        1 => {
+            let url = config.get(section.as_str().trim(), "url").unwrap();
+            println!("url: {}", url);
+            print!("New url: ");
+            stdout().flush().ok();
+            let new_url: String = read!(); 
+            let _url = config.set(section.as_str().trim(), "url", Some(new_url));
+            // parse_and_rewrite(section, line, data) logic.
+            // parse_and_rewrite(section.as_str(), 2, _url)
+        }
+        2 => {
+            let loc = config.get(section.as_str().trim(), "loc").unwrap();
+            println!("Location: {}", loc)
+        }
+        3 => {
+            let url = config.get(section.as_str().trim(), "url").unwrap();
+            let loc = config.get(section.as_str().trim(), "loc").unwrap();
+            println!("Url: {}\nLocation: {}", url, loc);
+        }
+        _ => println!("Failed to update configuration for your file."),
+    }
+    Ok(())
 }
-
 
 /// Read input for main menu.
 pub fn read_input_main() {
@@ -122,13 +149,21 @@ pub fn read_input_main() {
                 _ => println!("Failed to Update File."),
             }
             main_menu();
-        },
+        }
 
         3 => (),
 
         4 => {
             // Getting all information from overhaul.ini
             get_config();
+            main_menu();
+        }
+
+        5 => {
+            match edit_configuration() {
+                Ok(_) => println!("File Configured."),
+                _ => println!("Failed to edit configuration for your file."),
+            }
             main_menu();
         }
 
@@ -157,13 +192,16 @@ pub fn main_menu() {
     println!("[2] Update File.");
     println!("[3] Update All");
     println!("[4] Show all stored files.");
+    println!("[5] Edit config values.");
     print!("What would you like to do?: ");
     stdout().flush().ok();
     read_input_main();
 }
 
 pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,26 @@ pub mod dec;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-  //dec::main_menu();
-  let contents = String::from_utf8(fs::read(dec::LOG)?).unwrap();
-  for line in contents.lines() {
-    println!("{}", line);
-    let request = reqwest::get("https://swapi.dev/api/people").await?.text().await?;
+  #[allow(while_true)] // kek
+  while true {
+    dec::main_menu();
+    let _url = String::from_utf8(fs::read(dec::URL)?).unwrap(); // gets url from line in url.ini
+    for urls in _url.lines() { // loops through each line in url.ini
+      println!("Updating {}...", urls); //outputs urls
+
+      let mut file: &str; // location of file to be wirtten to, unassigned
+      let _loc = String::from_utf8(fs::read(dec::LOC)?).unwrap(); // location feched from loc.ini
+
+      for _location in _loc.lines() { // iterate through loc.ini
+        file = _location; // assigns location to line in loc.ini
+        let request = reqwest::get(urls).await?.text().await?; // requests the url currently in loop
+        let request = dec::store_request(request); // stores that request in some function i made idk
+        dec::write_to_file(file, request); // writes request data to file with another weird function i made somewhere kek
+      }
+    }
   }
   Ok(())
 }
+/*
+  Planning on adding a configuration section to create the config dir and all its files, fetch current path and put it in environment variables. But thats for a later date. This project has been fun.
+*/
